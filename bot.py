afa68a9bc6a0e9bc209b50a30580ec2cc657a304
flac2525 <las_zero@yahoo.co.jp>
2018-02-15 01:17:54 +0900

    なんやこれ　全然うまくいかないわあああああああああああああああ

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 import settings
 import minecraft_server_info
-import dice
+import dice2
 import asyncio
 
 client = discord.Client()
+dice = dice2.Dice()
 
 @client.event
 async def on_ready():
@@ -17,6 +18,7 @@ async def on_ready():
 async def on_message(message):
     utages = ['!うたげ', '!utage', '!ウタゲ']
     choco = ['!チョコ','!チョコレート']
+    args = message.content.split(' ') 
     if not message.author.bot:
         if message.content.startswith("おは"):
             m = "おはようですわよ" + message.author.name + "嬢！"
@@ -24,7 +26,7 @@ async def on_message(message):
             await client.send_message(message.channel, m)
         elif message.content in choco:
             # メッセージを書きます
-            m = "私の手作りチョコ受け取って貰えるかしら？" + message.author.name + "嬢"
+            m = "私の手作りチョコ受け取って貰えるかしら？" + message.author.name + "嬢。"
             # メッセージが送られてきたチャンネルへメッセージを送ります
             await client.send_message(message.channel, m)
         elif message.content.startswith("!mcnow "):
@@ -32,9 +34,11 @@ async def on_message(message):
             server_info = "<@%s> %s" % (message.author.id, minecraft_server_info.get_server_info(server_name))
             await client.send_message(message.channel, server_info)
         elif message.content.startswith("!dice "):
-            throw = message.content[6:]
-            diced = "<@%s> %s" % (message.author.id, dice.get_dice(throw))
-            await client.send_message(message.channel, diced)
+            dice.set_dice(args)#Dice型インスタンスにダイスの生文字列をセットしてやるわよ
+            await client.send_message(message.channel, dice.throw_dice())
+            #throw = message.content[6:]
+            #diced = "<@%s> %s" % (message.author.id, dice.get_dice(throw))
+            #await client.send_message(message.channel, diced)
         elif message.content in utages:
             await client.delete_message(message)
             # メッセージを書きます
--- dice2.py
+++ dice2.py
@@ -18,7 +18,7 @@ class Dice:
 		elif len(dice_text[1].split('<=')) > 1:
 			# throw_dice内の判定で使う less than フラグをTrueにしておく
 			self.lt_flag = True
-			# dice_text[1]のステータス値(1d100>=[50]の部分)を取得し、statusに保存
+			# dice_text[1]のステータス値(1d100<=[50]の部分)を取得し、statusに保存
 			self.status = int(dice_text[1].split('<=')[1])
 			# dice_text[1]のステータス値を保存したので、ステータス値を除外(1d100>=50→1d100)して再保存
 			dice_text[1] = dice_text[1].split('<=')[0]
@@ -36,15 +36,12 @@ class Dice:
 			dice_number.append(random.randint(1, int(self.num_surface)))
 			if self.lt_flag and dice_number[0] <= self.status or self.gt_flag and dice_number[0] >= self.status:
 				return str(dice_number) + ' 成功ですわよ！!'
+			if self.lt_flag and dice_number[0] <= self.status and self.num_surface is 100 and dice_number[0] <= 5:
+				return str(dice_number) + ' 成功しましたわよ！　なんとクリティカルですわ！'
 
 			else:
 				return str(dice_number) + ' 失敗ですわよ...。'
 
-			elif self.lt_flag and dice_number[0] <= self.status and self.num_surface is 100 and dice_number[0] <= 5:
-				dice_number = str(dice_number) + ' 成功しましたわよ！　なんとクリティカルですわ！'
-
-			elif self.lt_flag and dice_number[0] <= self.status and self.num_surface is 100 and dice_number[0] >= 95:
-				dice_number = str(dice_number) + ' 失敗しましたわよ...　しかもファンブルですわ...!'
 
 			return str(dice_number)
 		#else:
